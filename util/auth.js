924a165f23c0b7bd07f26c66d944f8ae3c65d05c
Afiq Aiman <afiqaimanafr@Afiqs-MacBook-Pro.local>
2022-06-14 14:09:28 +0800

    update: latest

--- util/auth.js
+++ util/auth.js
@@ -1,3 +1,14 @@
 import axios from "axios";
 
-function createUser() { }
+const API_KEY = 'AIzaSyD1LzpE1sMwVmzAc3je1hqIiOiHQA7Qu8g';
+
+async function createUser(email, password) {
+    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        });
+}
+
+export default createUser;
